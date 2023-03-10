662cb76b8e0a105e0ae51ad9a57c367b96944d9f
Nazar <furnoxl@yandex.ru>
2023-03-11 00:22:29 +0700

    Add api for currencies

--- src/firebase-service.js
+++ src/firebase-service.js
@@ -7,8 +7,8 @@ const firebaseConfig = {
     authDomain: process.env.VUE_APP_AUTH_DOMAIN,
     projectId: process.env.VUE_APP_PROJECT_ID,
     storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
